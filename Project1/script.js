a975d5e95054cfc52c6f5092968222e401ea50d9
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2014-07-01 20:13:10 -0400

    Update script.js

--- Project1/script.js
+++ Project1/script.js
@@ -26,13 +26,13 @@
 	// create the controller and inject Angular's $scope
 	myApp.controller('mainController', function($scope) {
 		// create a message to display in our view
-		$scope.message = 'Everyone come and see how good I look!';
+		$scope.message = 'I am using AngularJS Rounting!';
 	});
 
 	myApp.controller('aboutController', function($scope) {
-		$scope.message = 'Look! I am an about page.';
+		$scope.message = 'Welcome to the world od AngularJS!';
 	});
 
 	myApp.controller('contactController', function($scope) {
-		$scope.message = 'Contact us! JK. This is just a demo.';
+		$scope.message = 'AngularJS Templating';
 	});
