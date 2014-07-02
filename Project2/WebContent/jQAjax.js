62db6b7cc04e8998c4c8e67a6290cff29f377c33
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2014-07-02 04:27:20 -0400

    Create jQAjax.js

---
+++ Project2/WebContent/jQAjax.js
@@ -0,0 +1,55 @@
+"use strict";
+
+$(function() {
+
+    $('#getAll_btn').click(function() {
+
+        $.ajax({
+            aysnc: true,
+            cache: false,
+            dataType: 'json',
+            error: function(jqXHR, textStatus, error) {
+                console.log(error);
+            },
+            success: function(data, textStatus, jqXHR) {
+                console.log('SUCCESS', data);
+            },
+            type: 'GET',
+            url: 'http://localhost:8080/Eatery/rest/people/getAll'
+        });
+    });
+
+
+
+    $('#addPerson_btn').click(function() {
+
+        var dummyPerson = {
+            "firstName": "PRVN",
+            "lastName": "SALITRA",
+            "email": "contact@prvn.co",
+            "address1": "123 M St",
+            "address2": "",
+            "city": "Richmond",
+            "zip": 23228,
+            "state": "Virginia",
+            "phone": "1234567809"
+        };
+
+        $.ajax({
+            aysnc: true,
+            cache: false,
+            contentType: "application/json;charset=utf-8",
+            dataType: 'json',
+            data: JSON.stringify(dummyPerson),
+            error: function(jqXHR, textStatus, error) {
+                console.log(error);
+            },
+            success: function(data, textStatus, jqXHR) {
+                console.log('SUCCESS', data);
+            },
+            type: 'POST',
+            url: 'http://localhost:8080/Eatery/rest/people/add'
+        });
+    });
+
+});
