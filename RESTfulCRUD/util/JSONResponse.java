03c44a29cf0c640a9af793d4047d4db3857811b9
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2015-07-13 16:47:50 -0400

    Create JSONResponse.java

---
+++ RESTfulCRUD/util/JSONResponse.java
@@ -0,0 +1,35 @@
+package util;
+
+import java.util.HashMap;
+
+public class JSONResponse {
+	private String status; //SUCCESS, ERROR
+	private String msg;
+	private HashMap<String, Object> data;
+	
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	public HashMap<String, Object> getData() {
+		return data;
+	}
+	public void setData(HashMap<String, Object> data) {
+		this.data = data;
+	}
+	
+	public void setObject (String key, Object value){
+		if(data == null){
+			data = new HashMap<String, Object>();
+		}
+		data.put(key, value);
+	}
+}
